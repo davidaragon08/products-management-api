3fdc88e65a4ea4b9d551e5c21670f55f0d9263b6
David Aragon <aragondavid08@gmail.com>
2026-01-30 00:54:56 -0300

    chore(api): Configurar el registro, cors y canalización de middleware

--- ProductsManagement.Api/Program.cs
+++ ProductsManagement.Api/Program.cs
@@ -1,23 +1,61 @@
+using Microsoft.OpenApi.Models; // Add this using directive at the top of the file
+using ProductsManagement.Api.Middlewares;
+using ProductsManagement.Application.Interfaces;
+using ProductsManagement.Application.Services;
+using ProductsManagement.Domain.Interfaces;
+using ProductsManagement.Infrastructure.Repositories;
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200") 
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
+builder.Services.AddHealthChecks();
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Configure Dependency Injection
+builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi(); 
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
 
 app.Run();
